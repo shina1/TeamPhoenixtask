6d6cee7da6dac2ee2e91ac52c5caa79e388859e3
shina1 <oswasiu@gmail.com>
2019-09-15 17:50:49 +0100

    shina first commit

---
+++ land.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome|Land Page</title>
+</head>
+<body>
+<div class="container">	
+	<form action="#" method="POST">
+	<input type="text" name="firstName" placeholder="First Name">
+	<input tyep="text" name="lastName" placeholder="Last Name">
+	<input type="email" name="email" placeholder="you@mail.com">
+	<input type="password" name="password" placeholder="******">	
+	</form>
+</div>	
+</body>
+</html>

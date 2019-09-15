20339d3de9e11bb2fae8253773bb39f05336086d
shina1 <oswasiu@gmail.com>
2019-09-15 18:04:52 +0100

    the login page

--- land.php
+++ land.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Welcome|Land Page</title>
+	<title>Welcome|LogIn</title>
 </head>
 <body>
 <div class="container">	
 	<form action="#" method="POST">
-	<input type="text" name="firstName" placeholder="First Name">
-	<input tyep="text" name="lastName" placeholder="Last Name">
 	<input type="email" name="email" placeholder="you@mail.com">
-	<input type="password" name="password" placeholder="******">	
+	<input type="password" name="password" placeholder="******">
+	<input type="submit" name="submit" placeholder="Submit">
 	</form>
 </div>	
 </body>
